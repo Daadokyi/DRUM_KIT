fdf7c8ae70b1a9c75708018d151b403a485307a0
Daadokyi <Kwekudokyi@outlook.com>
2023-11-12 02:04:21 +0000

    sound feature fixed

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrums;  i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var beat = new Audio('Sounds\crash.mp3');
-        beat.play();
+        var audio = new Audio('Sounds/crash.mp3');
+        audio.play();
 
         
     });
