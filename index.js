87dca42254a1e2a92255ea731cb0ce3186ddad34
Daadokyi <Kwekudokyi@outlook.com>
2023-11-15 21:38:21 +0000

    implementing switch

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrums;  i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var audio = new Audio('Sounds/crash.mp3');
+        var buttonInnerHtml = this.innerHTML;
+        switch (buttonInnerHtml) {
+            case "w":
+                var audio = new Audio('Sounds/crash.mp3');
         audio.play();
-
+                break;
+        
+            default:
+                break;
+        }
         
     });
 }
+
 
 
 
