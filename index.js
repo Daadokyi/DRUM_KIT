76ccf7350cfb90f747cc3e57ce34bc34864a336b
Daadokyi <Kwekudokyi@outlook.com>
2023-11-16 08:23:35 +0000

    drum kit functionality added

--- index.js
+++ index.js
@@ -5,12 +5,41 @@ for (var i = 0; i<numberOfDrums;  i++){
         var buttonInnerHtml = this.innerHTML;
         switch (buttonInnerHtml) {
             case "w":
-                var audio = new Audio('Sounds/crash.mp3');
-        audio.play();
+                var crash = new Audio('Sounds/crash.mp3');
+                crash.play();
                 break;
-        
+
+            case "a":
+                var kick = new Audio('Sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            
+            case "s":
+                var snare = new Audio('Sounds/snare.mp3');
+                snare.play();
+                break;
+            
+            case "d":
+                var tom1 = new Audio('Sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "j":
+                var tom2 = new Audio('Sounds/tom-2.mp3');
+                tom2.play();
+                break;
+                
+             case "k":
+                var tom3 = new Audio('Sounds/tom-3.mp3');
+                tom3.play();
+                break;
+                
+            case "l":
+                var tom4 = new Audio('Sounds/tom-4.mp3');
+                tom4.play();
+                break;    
             default:
-                break;
+               
         }
         
     });
